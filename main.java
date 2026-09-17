bdd0e7fc2ad20973a1ecb6611fbc258b7e9f8e9f
AdryenMendes <adryenmendes2016@gmail.com>
2026-09-17 07:27:21 -0300

    primeiro commit

---
+++ main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Personagem p1 = new Personagem("Aragorn", 100, 20);
+        Personagem p2 = new Personagem("Orc", 50, 10);
+
+        p1.atacar(p2);
+        
+        // AQUI ESTÁ O PROBLEMA: Como os atributos são públicos,
+        // eu posso fazer algo impossível matematicamente ou bizarro:
+        p2.vida = -500; 
+        p2.forca = 999999; 
+
+        System.out.println("Vida do Orc: " + p2.vida);
+    }
+}
