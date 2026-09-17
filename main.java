80a2150811b3c0d907f8125c690fb78d13fb0787
Adryen Mendes da Silva <adryenmendes@usp.br>
2026-09-17 11:13:19 -0300

    adaptações para abstract e personagens decidirem o ataque

--- main.java
+++ main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Personagem p1 = new Personagem("Aragorn", 100, 20);
-        Personagem p2 = new Personagem("Orc", 50, 10);
+        // Como Personagem agora é abstract, não podemos dar "new Personagem"
+        // Temos que criar um Guerreiro ou Mago
+        Personagem p1 = new Guerreiro("Aragorn", 100, 20, 30);
+        Personagem p2 = new Guerreiro("Orc", 50, 10, 0);
 
         p1.atacar(p2);
 
-        System.out.println("Vida do Orc: " + p2.vida);
+        // ERRO CORRIGIDO: Usamos getVida() em vez de .vida
+        System.out.println("Vida do Orc: " + p2.getVida());
     }
 }
--- personagem.java
+++ personagem.java
@@ -1,37 +1,35 @@
-public class Personagem {
+public abstract class Personagem { // Vamos tornar abstrata como planejamos!
     private String nome;
     private int vida;
     private int forca;
 
     public Personagem(String nome, int vida, int forca) {
-        this.nome = nome;                                              // parametro a recebe valor no a
-        this.vida = vida;       
+        this.nome = nome;
+        this.vida = vida;
         this.forca = forca;
     }
 
-    public int getVida()  { return this.vida;  }
-    public string getNome() { return this.nome; }
+    public int getVida()  { return this.vida; }
+    public String getNome() { return this.nome; } 
     public int getForca()  { return this.forca; }
 
-    
-    // receber dano
+
+    // mecaninca de receber dano
     public void receberDano(int quantidade) {
         if (quantidade < 0) {
-            system.out.println("Erro: dano nÃ£o pode ser negativo")
-            return
+            System.out.println("Erro: dano não pode ser negativo");
+            return; 
         }
         this.vida -= quantidade;
 
-        if (this.vida < 0){
-            this.vida = 0
-            system.out.println(this.nome + "foi derrotado")
+        if (this.vida < 0) {
+            this.vida = 0;
+        }
+        if (this.vida == 0) {
+            System.out.println(this.nome + " foi derrotado!"); 
         }
     }
 
-
-    // ataque simples
-    public void atacar(Personagem alvo) {                               // ex: paladino dragao
-        System.out.println(this.nome + " atacou " + alvo.nome);         // printar "paladino atacou dragao"
-        alvo.vida = alvo.vida - this.forca;                             // vida do alvo = vida base - dano
-    }
+    // abstract pra os filhos decidirem o ataque
+    public abstract void atacar(Personagem alvo); 
 }
